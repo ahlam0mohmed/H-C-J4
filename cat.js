410d3c9385e911d1c0f2cddcf9a0dbdb47642dd0
Ahlam Mohmmed <ahlam93mm@gmail.com>
2022-01-27 08:19:08 +0300

    first lode

---
+++ cat.js
@@ -0,0 +1,29 @@
+var pre_but =document.getElementById('but_left');
+var nex_but =document.getElementById('but_right');
+var change =document.querySelectorAll(".modimg");
+var imgs =document.getElementById('imgb');
+
+var index=0;
+
+nex_but.addEventListener('click',function(){
+    
+    if(index>=change.length){
+        index=0;
+    }
+    
+    imgs.src= change[index].src;
+    index++;
+    
+});
+
+pre_but.addEventListener('click',function(){
+    
+    
+    if(index<=0){
+        index=change.length;
+    }
+    index--;
+    imgs.src= change[index].src;
+    
+    
+});
